9856f3f1123e4b3b763f3fbd8fb6b315029334f0
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-06-22 23:19:57 +0300

    Update user-roles.controller.ts

--- src/user-roles/user-roles.controller.ts
+++ src/user-roles/user-roles.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger/dist';
 import { UserRolesService } from './user-roles.service';
 import { CreateUserRoleDto } from './dto/create-user-role-dto';
 import { UserRole } from './user-roles.model';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiTags('User Roles')
 @Controller('user-roles')
@@ -13,6 +22,7 @@ export class UserRolesController {
   @ApiOperation({ summary: 'Create user role' })
   @ApiResponse({ status: 201, type: UserRole })
   @UsePipes(ValidationPipe)
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() dto: CreateUserRoleDto) {
     return this.userRolesService.createUserRole(dto);
@@ -20,6 +30,7 @@ export class UserRolesController {
 
   @ApiOperation({ summary: 'Get user role' })
   @ApiResponse({ status: 200, type: UserRole })
+  @UseGuards(JwtAuthGuard)
   @Get('/:value')
   getByValue(@Param('value') value: string) {
     return this.userRolesService.getUserRoleByValue(value);
