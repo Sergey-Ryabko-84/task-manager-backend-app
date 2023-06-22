a9ee44e7410a4bd1c6566548e46b16bd191d4eb1
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-06-23 00:11:36 +0300

    Update user-roles.module.ts

--- src/user-roles/user-roles.module.ts
+++ src/user-roles/user-roles.module.ts
@@ -5,11 +5,15 @@ import { UserRolesController } from './user-roles.controller';
 import { UserRole } from './user-roles.model';
 import { User } from 'src/users/users.model';
 import { UserRolesCorresponds } from './user-roles-corresponds.model';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [UserRolesService],
   controllers: [UserRolesController],
-  imports: [SequelizeModule.forFeature([UserRole, User, UserRolesCorresponds])],
+  imports: [
+    SequelizeModule.forFeature([UserRole, User, UserRolesCorresponds]),
+    AuthModule,
+  ],
   exports: [UserRolesService],
 })
 export class UserRolesModule {}
